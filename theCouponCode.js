8a4cbea9bd6ecf6a8ca9364899352bbea71e6752
yerbol kosbayev <yerbolkosbayev@gmail.com>
2019-10-10 16:40:33 +0200

    minor changes

--- theCouponCode.js
+++ theCouponCode.js
@@ -1,10 +1,9 @@
 // Task
 // Your mission:
 // Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
-
 // A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
-
+// Your function should return true or false
 
 exports.checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => {
-  
+
 }
