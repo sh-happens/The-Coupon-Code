b84daee657705d33e654b630451488fe40cc2d09
Bernat Muntaner <muntanerbernat@gmail.com>
2019-10-10 19:54:43 +0200

    done

--- theCouponCode.js
+++ theCouponCode.js
@@ -5,5 +5,25 @@
 // Your function should return true or false
 
 exports.checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => {
+  if (enteredCode !== correctCode) return false;
+  obj = {
+    January: 1,
+    February: 2,
+    March: 3,
+    April: 4,
+    May: 5,
+    June: 6,
+    July: 7,
+    August: 8,
+    September: 9,
+    October: 10,
+    November: 11,
+    December: 12
+  };
+  const newCurr = currentDate.split(' ');
+  const newExp = expirationDate.split(' ');
 
+  if (Number(newExp[2]) < Number(newCurr[2])) return false;
+  if (obj[newExp[0]] < obj[newCurr[0]]) return false;
+  return true;
 }
